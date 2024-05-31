0147e7fb8506387b083a80980a82103ce6262b5e
Falcon <nalindard@gmail.com>
2024-05-31 23:32:46 +0530

    Finalyze

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css'
 import { ReactNode } from 'react';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 interface RootLayoutProps {
   children: ReactNode
